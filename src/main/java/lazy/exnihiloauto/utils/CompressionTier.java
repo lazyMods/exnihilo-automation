850d6e09ac6f3765b6cf9211610c5b61630d206a
lazynessmind <lazynessmind@gmail.com>
2021-03-10 22:26:41 +0000

    Add: ReinforcedHammerItem and reinforce hammer tool type.

--- src/main/java/lazy/exnihiloauto/utils/CompressionTier.java
+++ src/main/java/lazy/exnihiloauto/utils/CompressionTier.java
@@ -1,16 +1,21 @@
 package lazy.exnihiloauto.utils;
 
+import javax.annotation.Nullable;
+
 public enum CompressionTier {
 
-    COMPRESSED(9, "Compressed "),
-    HIGHLY_COMPRESSED(81, "Highly Compressed "),
-    ATOMIC_COMPRESSION(729, "Atomic Compressed ");
+    COMPRESSED(9, "Compressed ", null),
+    HIGHLY_COMPRESSED(81, "Highly Compressed ", COMPRESSED),
+    ATOMIC_COMPRESSION(729, "Atomic Compressed ", HIGHLY_COMPRESSED);
 
     public int tierAmt;
     public String name;
+    @Nullable
+    public CompressionTier tierBelow;
 
-    CompressionTier(int tierAmt, String name) {
+    CompressionTier(int tierAmt, String name, @Nullable CompressionTier tierBelow) {
         this.tierAmt = tierAmt;
         this.name = name;
+        this.tierBelow = tierBelow;
     }
 }

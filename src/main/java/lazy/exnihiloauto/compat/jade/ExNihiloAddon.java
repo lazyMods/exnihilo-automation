db2027e3e249f47c57970351b4544dd2605a693f
lazynessmind <52864251+lazynessmind@users.noreply.github.com>
2021-06-28 22:47:27 +0100

    Basic Jade Addon

---
+++ src/main/java/lazy/exnihiloauto/compat/jade/ExNihiloAddon.java
@@ -0,0 +1,63 @@
+package lazy.exnihiloauto.compat.jade;
+
+import mcp.mobius.waila.api.*;
+import net.minecraft.entity.Entity;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.ResourceLocation;
+
+public class ExNihiloAddon implements IRegistrar {
+    @Override
+    public void addConfig(ResourceLocation resourceLocation, boolean b) {
+
+    }
+
+    @Override
+    public void addSyncedConfig(ResourceLocation resourceLocation, boolean b) {
+
+    }
+
+    @Override
+    public void registerStackProvider(IComponentProvider iComponentProvider, Class aClass) {
+
+    }
+
+    @Override
+    public void registerComponentProvider(IComponentProvider iComponentProvider, TooltipPosition tooltipPosition, Class aClass) {
+
+    }
+
+    @Override
+    public void registerBlockDataProvider(IServerDataProvider<TileEntity> iServerDataProvider, Class aClass) {
+
+    }
+
+    @Override
+    public void registerOverrideEntityProvider(IEntityComponentProvider iEntityComponentProvider, Class aClass) {
+
+    }
+
+    @Override
+    public void registerEntityStackProvider(IEntityComponentProvider iEntityComponentProvider, Class aClass) {
+
+    }
+
+    @Override
+    public void registerComponentProvider(IEntityComponentProvider iEntityComponentProvider, TooltipPosition tooltipPosition, Class aClass) {
+
+    }
+
+    @Override
+    public void registerEntityDataProvider(IServerDataProvider<Entity> iServerDataProvider, Class aClass) {
+
+    }
+
+    @Override
+    public void registerDecorator(IBlockDecorator iBlockDecorator, Class aClass) {
+
+    }
+
+    @Override
+    public void registerTooltipRenderer(ResourceLocation resourceLocation, ITooltipRenderer iTooltipRenderer) {
+
+    }
+}
